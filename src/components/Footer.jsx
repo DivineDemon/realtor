936167306423a026d4ffac08f053beb6e17f8a4e
DivineDemon <supame123@gmail.com>
2023-06-14 19:18:30 +0500

    Revert "UI Complete! ✨🚀💄"
    This reverts commit 1d4bb1e8928b2a3b4e42e2f4e5cb4ab9d35fc261.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="bg-black py-8 text-center text-white">
-      <div className="container mx-auto">
-        Copyright &copy; 2023. All Rights Reserved.
-      </div>
-    </footer>
-  );
+  return <div>Footer</div>;
 };
 
 export default Footer;
