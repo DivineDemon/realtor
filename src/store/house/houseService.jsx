a973ed4fa57c28917a86f76b4c43a78916995c16
DivineDemon <supame123@gmail.com>
2023-06-15 20:32:27 +0500

    :hammer:

--- src/store/house/houseService.jsx
+++ src/store/house/houseService.jsx
@@ -1,5 +1,7 @@
 const getHouses = async () => {
-  const response = await fetch(`${process.env.REACT_APP_HOUSE_ENDPOINT}`);
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_HOUSE_ENDPOINT}`
+  );
   const data = await response.json();
   return data.response;
 };
